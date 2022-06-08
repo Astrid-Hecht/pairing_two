c55a29459915f9e975e8a1c68b35543f22c2a3d5
Astrid Hecht <silentaviary@gmail.com>
2022-06-08 13:51:08 -0600

    Add new array method

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Astrid","Shawn","Mary", "Sid","Amy"]
 puts arrays[2]
 puts arrays[arrays.length-1]
 puts arrays[0]
+
+places = ["denver","boulder", "aurora", "broomfield","littleton"]
+places.unshift("ft collins")
+p places.sort
